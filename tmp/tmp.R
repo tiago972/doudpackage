fd0ca1d6b2d1ad475246ed5fa9d707460216a348
tiago972 <edouard.baudouin@hotmail.fr>
2021-02-07 01:09:13 +0100

    okk ca sera donc kable extra; il faut faire une fonction pour créer les arguments n il y a un problème avec min.max (il y a les mêmes valeurs pour le sous groupe 1 et total)

--- tmp/tmp.R
+++ tmp/tmp.R
@@ -1,17 +1,4 @@
-banana <- iris[1, ]
-banana[1,] <- NA
-iris
-t1 <- seq(from=0, to=nrow(iris), by=5)
-t2 <- seq(from=1, to=nrow(iris), by=5)
-
-ref <- data.frame(from=t2, to=t1[-1])
-
-tmp <- c()
-for(i in 1:nrow(ref)){
-  tmp2 <- rbind(iris[ref[i, "from"]:ref[i,"to"],], banana)
-  print(tmp2)
-  tmp <- rbind(tmp, tmp2)
-}
+### Data sample for testing #####
 
 test_map1<-data.frame("nom" = rep(LETTERS[1:10], each=2, length.out=20),
                                       "num" = rep(1:10, each=2, length.out=20),
@@ -19,18 +6,8 @@ test_map1<-data.frame("nom" = rep(LETTERS[1:10], each=2, length.out=20),
 test_map1
 test_map2<-data.frame("nom" = rep(LETTERS[1:10]), "num" = rep(11:20), group = rep("0", 10))
 test_map2
-test_tmp<-c()
-i = 1;
-j = 1;
-while (i < nrow(test_map1))
-{
-  test_tmp2<-rbind(test_map1[i,], test_map2[j,])
-  test_tmp2<-rbind(test_tmp2, test_map1[i+1,])
-  test_tmp<-rbind(test_tmp, test_tmp2)
-  i = i + 2;
-  j = j +1;
-}
-grepl(pattern = "^Missing values.*", test3$var)
-test4<-test3[!grepl(pattern = "^Missing values.*", test3$var) && test3$Total=="0(0)",]
-view(test4)
+################ Test pour htmlTable ######
+output <- matrix(1:4,ncol=2,dimnames = list(list("Row 1", "Row 2"),list("Column 1", "Column 2")))
+
+class(htmlTable(output))
 
