05aa8e976a10666ab2820dd6a5c1328704d6c31f
tiago972 <edouard.baudouin@hotmail.fr>
2021-05-28 23:02:26 -0400

    debog

--- tmp/debog.R
+++ tmp/debog.R
@@ -1,4 +1,6 @@
-setwd("C:/Users/4092931/Documents/GitHub/doudpackage/R")
+# setwd("C:/Users/4092931/Documents/GitHub/doudpackage/R")
+setwd("/Users/Tiago/Desktop/BF is cool/doudpackage/R")
+
 source('./ft_quali.R')
 source('./ft_quanti.R')
 source('./tools.R')
@@ -6,21 +8,20 @@ source('./ft_ana_biv.R')
 source('./ft_quanti.R')
 source('./ft_univ_tab.R')
 source('./ft_parse.R')
-setwd("C:/Users/4092931/Documents/GitHub/Scripts.PBR/Lea")
-source('./Lea.R')
-setwd("C:/Users/4092931/Documents/GitHub/doudpackage/tmp")
+# setwd("C:/Users/4092931/Documents/GitHub/scripts.PBR/Lea")
+setwd("/Users/Tiago/Desktop/BF is cool/Judith Breth")
+source('./judith.R')
 
-bdd<-droplevels(bdd[!bdd$Mortalite.a.J30 == 'ehpad les grands cedres',])
-a<-ft_desc_tab(bdd, group = "Mortalite.a.J30",complete = F, quali = T)
-res_a<-ft_parse(a, bdd, group="Mortalite.a.J30", col.order = c(3,4), group.name = c("0","1"))
+a<-ft_desc_tab(bdd, group = "DC.a.6mois",complete = F, quali = T)
+res_a<-ft_parse(a, bdd, group="vivant.s4", col.order = c(3,4), group.name = c("0","1"))
 res_a
-b<-ft_desc_tab(bdd, group = "Mortalite.a.J30",complete = F, quanti = T)
-res_b<-ft_parse(b, bdd, group="Mortalite.a.J30", col.order = c(3,4), group.name = c("0","1"))
+b<-ft_desc_tab(bdd, group = "DC.a.6mois",complete = F, quanti = T)
+res_b<-ft_parse(b, bdd, group="vivant.s4", col.order = c(3,4), group.name = c("0","1"))
 res_b
 
-c<-ft_desc_tab(bdd, group = "Mortalite.a.J30")
+c<-ft_desc_tab(bdd, group = "DC.a.6mois")
 c
-res_c<-ft_parse(c, bdd, group="Mortalite.a.J30", col.order = c(3,4), group.name = c("0","1"))
+res_c<-ft_parse(c, bdd, group="vivant.s4", col.order = c(3,4), group.name = c("0","1"))
 res_c
 
 
