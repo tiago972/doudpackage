343272c8d7afc1a0034f986103cfdb026325212c
tiago972 <edouard.baudouin@hotmail.fr>
2021-09-12 23:05:25 +0200

    cf log precedent

--- R/ft_parse.R
+++ R/ft_parse.R
@@ -21,7 +21,7 @@ ft_name_col<-function(res, data, group, digits.opt)
 #'  Output is generated in HTML by KableExtra
 #' @param res Outpul of ft_desc_tab
 #' @param data A database, dataframe object
-#' @param col.order A vector containing column indexes or names to use for column order
+#' @param col.order A vector containing levels of group according to the desired order
 #' @param group The name of the group used in ft_desc_tab
 #' @param group.name Vector containing subgroup names
 #' @param digits.opt How many numbers to display after the ".". Should be the same as provided in ft_desc_tab, default is 0
@@ -30,16 +30,19 @@ ft_name_col<-function(res, data, group, digits.opt)
 #' @export
 ft_parse<-function(res, data, group, col.order = NULL, group.name = NULL, digits.opt = 1)
 {
+  ## rajouter gestion des erreurs: si col.order ne correspond à aucun level de group
   data<-data[!is.na(data[,group]),]
  if (!is.null(col.order)) # ligne ci dessous a changer +++ en fonction de col.order
-   res<-res[,c(which(colnames(res) == "var"), which(colnames(res) =="Total"), which(colnames(res) == group.name[1]), which(colnames(res) == group.name[2]), which(colnames(res) =="p"))]
+   res<-res[,c(which(colnames(res) == "var"), which(colnames(res) =="Total"), which(colnames(res) == col.order[1]), which(colnames(res) == col.order[2]), which(colnames(res) =="p"))]
  else
    res<-res[,c("var","Total",3,4,"p")]
-  if (is.null(group.name))
+  if (!is.null(group.name))
  {
-   group.name[1]<-colnames(res[,3])
-   group.name[2]<-colnames(res[,4])
+   colnames(res[,3])<-group.name[1]
+   colnames(res[,4])<-group.name[2]
   }
+ else
+   group.name = levels(data[,group])
 res<-ft_name_col(res, data, group, digits.opt)
 
 ident<-grep("Missing values.*", res$var)
