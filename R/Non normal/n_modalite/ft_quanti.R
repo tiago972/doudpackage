099b623e2ce6ac9b54a6f8c37ff29d98a91fbcb7
tiago972 <edouard.baudouin@hotmail.fr>
2022-03-25 11:09:49 +0100

    pas encore d'idée pour éviter la boucle

--- R/Non normal/n_modalite/ft_quanti.R
+++ R/Non normal/n_modalite/ft_quanti.R
@@ -2,15 +2,15 @@ digits.opt = 1
 nonnormal = 0
 
 ft_tab_quanti<-function(col.var, digits.opt = 1, nonnormal = 0){
-  print(names(col.var))
   if(!is.numeric(col.var))
     return(NA)
+  print(names(col.var))
   "Min"<-min(col.var, na.rm=TRUE)
   "Max"<-max(col.var, na.rm=TRUE)
   if (nonnormal == 0){
     "mean"<-round(mean(col.var, na.rm = T), digits = digits.opt)
     "sd"<-round(sd(col.var, na.rm = T), digits = digits.opt)
-    tmp_mat<-c(var = colnames(data)[i],
+    tmp_mat<-c(var = names(col.var),
                "Total"=gsub(" ", "", paste(mean, "(",  sd,")")), "Min-Max"=paste(Min, Max, sep="-"))
   }
   print(tmp_mat)
@@ -21,11 +21,9 @@ ft_runquanti_tab<-function(data, group, digits.opt, nonnormal){
   # if(is.null(group))
   #   return(ft_tab_quanti_null())
   # else{
-  res<-data.frame(unlist(by(data[,"Age"], data[,group], ft_tab_quanti, simplify = F), use.names = F))
+    res<-sapply(bdd[,'Age'], bdd[,"Sexe"], ft_tab_quanti, digits.opt, nonnormal, simplify = T)
+
     return(res)
   # }
 }
 a<-ft_runquanti_tab(bdd, group = "Sexe", digits.opt, nonnormal)
-b<-ft_runquanti_tab(bdd, group = "Sexe", digits.opt, nonnormal)
-
-test<-split(bdd, bdd$Sexe)
