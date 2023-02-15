45363174e7ea97a4b23646139bf3306c6900b068
tiago972 <edouard.baudouin@hotmail.fr>
2023-02-15 22:56:04 +0100

    encore une correction sur complete/quali/quanti

--- R/ft_univ_tab.R
+++ R/ft_univ_tab.R
@@ -29,7 +29,6 @@ ft_desc_tab<-function(data, group=NULL, complete = TRUE, quanti=FALSE, quali=FAL
     warning(paste(table(data[,group], useNA = "always")[nlevels(data[,group]) + 1], " rows have been deleted due to missing values in the defined group" ,sep = ""))
     data<-data[!is.na(data[,group]),]
   }
-
   names.tmp<-sapply(data, class)
   if (length(names(names.tmp[names.tmp %in% "factor"])) == 1){
     quali = FALSE
@@ -43,12 +42,13 @@ ft_desc_tab<-function(data, group=NULL, complete = TRUE, quanti=FALSE, quali=FAL
   }
   else
     quanti = TRUE
-  if (isTRUE(quanti)||isTRUE(quali))
-    complete=FALSE
+  if (isTRUE(quanti) && isTRUE(quali))
+    complete=TRUE
   if (isTRUE(complete) || isTRUE(quanti))
     quanti_tab<-ft_parse_quanti_opt(ft_quanti(data, group, p.value, min.max, na.print, digits.opt, nonnormal), min.max, na.print, p.value, group, nonnormal)
   if (isTRUE(complete) || isTRUE(quali))
      quali_tab<-ft_parse_quali_opt(ft_quali(data, group, p.value, na.print, digits.opt), na.print, p.value, group)
+  print(paste(complete, quali, quanti))
   if (!isTRUE(complete) && isTRUE(quanti))
     return(quanti_tab)
   else if (!isTRUE(complete) && isTRUE(quali)){
