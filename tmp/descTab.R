611bffcd6264568966b46a560e4d0204a3c05465
tiago972 <edouard.baudouin@hotmail.fr>
2023-02-19 11:48:38 +0100

    git status

--- tmp/descTab.R
+++ tmp/descTab.R
@@ -19,13 +19,12 @@ descTab<-function(data, group=NULL, quanti=TRUE, quali=TRUE, na.print = FALSE,
                       p.value=TRUE, min.max=FALSE, digits.p=3, digits.qt = 1, 
                   digits.ql = 1, normality = "normal")
 {
-  var_list<-varType(data, nonnormal)
+  var_list<-varType(data, normality)
   ana.biv_list<-anaBiv(var_list, data = data, group = group, digits.p = digits.p)
   if (is.null(ana.biv_list))
     ana.biv_list<-lapply(var_list@List, as, Class = "VarGroup")
   ana.biv_list<-new("listVar", List = ana.biv_list)
-  ana.univ_list<-anaUniv(ana.biv_list, group = group, 
+  ana.univ_list<-anaUniv(ana.biv_list, group = group,
                          data = data, digits.qt = digits.qt, digits.ql = digits.ql)
-  
   return(ana.univ_list)
 }
--- tmp/test_file.R
+++ tmp/test_file.R
@@ -7,9 +7,14 @@ iris$fact_2<-as.factor(as.character(sample(1:2, 150, replace = TRUE)))
 n_na<-sample(1:150, 10)
 iris[n_na, "fact_2"]<-NA
 
+iris$fact_3<-as.factor(as.character(stri_rand_strings(150, 1, '[A-B]')))
+
 summary(iris)
 
 iris_test<-descTab(iris, group = "Species")
+ana.tmp<-anaUniv(iris_test, group = "Species", data = iris, digits.qt = 1, digits.ql = 1)
+
+
 test<-makeTable(iris_test, group = "Species")
 rm(tmp_df)
 
